b53323ee7200d70225b3532540b52780c5a29655
Vinícius Calasans <vinicius.calasans@hotmail.com>
2019-06-13 20:49:36 -0300

    Fix getSession call

--- node/handlers/catalog.ts
+++ node/handlers/catalog.ts
@@ -11,9 +11,11 @@ export const catalog = async (ctx: Context) => {
   const {vtex: {account, authToken, operationId, production, route: {params: {path}}, segmentToken, sessionToken}, query, method} = ctx
   let VtexIdclientAutCookie: string | undefined
 
+  console.log(JSON.stringify(ctx, null, 2))
   if (sessionToken) {
     const { session } = ctx.clients
-    const sessionPayload = await session.getSession(sessionToken, [])
+    const sessionPayload = await session.getSession(sessionToken, ['*'])
+    console.log(JSON.stringify(sessionPayload, null, 2))
     VtexIdclientAutCookie = ramdaPath(['namespaces', 'cookie', `VtexIdclientAutCookie_${account}`], sessionPayload)
   }
   console.log(`VtexIdclientAutCookie: ${VtexIdclientAutCookie}`)
