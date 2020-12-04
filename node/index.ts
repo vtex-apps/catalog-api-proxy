3c9cd698a3cda48999bd1520211fee0529a05b1e
Victor Elias <victorgelias@gmail.com>
2020-12-03 21:47:13 -0300

    Fix index.ts exports
    Was quite fun to make that compose stuff xD

--- node/index.ts
+++ node/index.ts
@@ -3,19 +3,20 @@ import { request } from './middlewares/request'
 
 process.env.DETERMINISTIC_VARY = 'true'
 
-const TWO_SECONDS_MS =  2 * 1000
-
-export default new Service({
-  clients: {
-    options: {
-      default: {
-        retries: 1,
-        timeout: TWO_SECONDS_MS,
-      },
-    }
-  },
-  routes: {
-    catalog: [prepare(false), request],
-    authenticatedCatalog: [prepare(true), request],
+function compose(...middlewares: Array<(ctx: any, next: () => Promise<void>) => Promise<void>>): (ctx: any) => Promise<void> {
+  if (middlewares.length === 0) {
+    return () => Promise.resolve()
   }
-})
+
+  const next = compose(...middlewares.slice(1))
+  return (ctx: any) => {
+    return middlewares[0](ctx, () => next(ctx))
+  }
+}
+
+export default {
+  routes: {
+    catalog: compose(prepare(false), request),
+    authenticatedCatalog: compose(prepare(true), request),
+  }
+}
